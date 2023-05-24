e8e4fcba77d8210991a706a2e938eebbe708ff2e
Brad Chou <brad.chow@gmail.com>
2023-05-24 20:49:54 +0800

    Update shin_kong_mitsukoshi.py
    To correct format of README.md

--- department_store_brands/department_store_brands/spiders/shin_kong_mitsukoshi.py
+++ department_store_brands/department_store_brands/spiders/shin_kong_mitsukoshi.py
@@ -6,22 +6,28 @@ class ShinKongMitsukoshiSpider(scrapy.Spider):
     original_url = "https://www.skm.com.tw/brand_list/?brand_capital=&brand_category=&query=&page="
     page_num = 1
     start_urls = [ original_url + (str)(page_num)  ]
+    OUTPUT_TO_MD = 1
     
     def parse(self, response):
-        print("Parse for: " + response.url)
+        if self.OUTPUT_TO_MD == 0:
+            print("Parse for: " + response.url)
 
         brands_num = len(response.xpath('//div[@class="grid-container grid-lg-4 grid-md-3 grid-xxs-2"]/div'))
         if (brands_num > 0):
             infos = response.xpath('//div[@class="grid-container grid-lg-4 grid-md-3 grid-xxs-2"]/div')
             for info in infos:
-                print(info.xpath('.//a/div/text()').get())
-                content += "[" + info.xpath('.//a/div/text()').get() + "]"
-                print(info.xpath('.//a/@href').get())
-                content += "(" + info.xpath('.//a/@href').get() + ")\n"
+                if self.OUTPUT_TO_MD == 0:
+                    print(info.xpath('.//a/div/text()').get())
+                if self.OUTPUT_TO_MD == 1:
+                    print("[" + info.xpath('.//a/div/text()').get() + "](" + info.xpath('.//a/@href').get() + ")  ")
+                if self.OUTPUT_TO_MD == 0:
+                    print(info.xpath('.//a/@href').get())
                 locations = info.xpath('.//div/ul/li')
                 for location in locations:
-                    print("	" + "location: " + (str)(location.xpath('.//text()').get().replace('\n', '').replace('\t', '').replace(' ', '').split('\xa0')))
-                    content += (str)(location.xpath('.//text()').get().replace('\n', '').replace('\t', '').replace(' ', '')) + "\n"
+                    if self.OUTPUT_TO_MD == 0:
+                        print("	" + "location: " + (str)(location.xpath('.//text()').get().replace('\n', '').replace('\t', '').replace(' ', '').split('\xa0')))
+                    if self.OUTPUT_TO_MD == 1:
+                        print((str)(location.xpath('.//text()').get().replace('\n', '').replace('\t', '').replace(' ', '')) + " " + " ")
 #                    print("	" + "location: " + (str)(location.xpath('.//text()').get().replace('\n', '').replace('\t', '').replace(' ', '').split('\xa0')[0]))
 #                    print("	" + "floor: " + (str)(location.xpath('.//text()').get().replace('\n', '').replace('\t', '').replace(' ', '').split('\xa0')[1]))
             #Yield for next page
@@ -29,7 +35,5 @@ class ShinKongMitsukoshiSpider(scrapy.Spider):
             url = self.original_url + (str)(self.page_num)
             yield scrapy.Request(url, callback=self.parse)
         else:
-            print("No brands at: " + (str)(response.url))
-        
-        with open('../README.md', 'a') as out:
-            out.write(content)
+            if self.OUTPUT_TO_MD == 0:
+                print("No brands at: " + (str)(response.url))
